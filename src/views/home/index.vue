069185bea8354f0e3275aa27885c4bd81975315f
hehao00 <13663474696@163.com>
2019-07-09 16:20:06 +0800

    登陆页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p>123</p>
+    <p>456</p>
   </div>
 </template>
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,30 @@
 <template>
   <div>
-    <p>123</p>
+      <!-- 头部 -->
+      <van-nav-bar class="nav-bar" title="登陆"/>
+      <!--表单 -->
+      <van-cell-group>
+        <van-field
+            v-model="user.mobile"
+            required
+            clearable
+            label="手机号"
+            placeholder="请输入手机号"
+            @click-right-icon="$toast('question')"
+        />
+        <van-field
+            v-model="user.code"
+            type="password"
+            label="验证码"
+            placeholder="请输入验证码"
+            required
+        />
+     </van-cell-group>
+     <!-- 登陆按钮 -->
+     <div class="login-btn">
+         <van-button class="btn" type="info">登陆</van-button>
+     </div>
+
   </div>
 </template>
 
@@ -10,6 +34,10 @@ export default {
 
   data () {
     return {
+      user: {
+        mobile: '',
+        code: ''
+      }
     }
   },
 
@@ -18,5 +46,17 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+.van-nav-bar__title {
+    color:#fff;
+}
+.nav-bar {
+    background-color: #3296fa;
+}
+.btn {
+    width:100%;
+    border-radius: 5px;
+}
+.login-btn {
+    padding:20px;
+}
 </style>
