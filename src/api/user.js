fff22bc7f897f2f725b276cedde1cd45dc25ba32
hehao00 <13663474696@163.com>
2019-07-09 16:39:52 +0800

    封装api请求函数

---
+++ src/api/user.js
@@ -0,0 +1,13 @@
+// 用户相关接口封装模块
+import request from '@/utils/request'
+
+export const login = ({ mobile, code }) => {
+  return request({
+    method: 'POST',
+    url: '/app/v1_0/authorizations',
+    data: {
+      mobile,
+      code
+    }
+  })
+}
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 // 封装axios请求模块
+// axios.create用于创建一个axios 实例
 const request = axios.create({
   baseURL: 'http//toutiao.course.itcast.cn'
 })
