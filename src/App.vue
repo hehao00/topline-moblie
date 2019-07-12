469f8ac0bf7c81d2712dfd214c46d5ef8e9fa040
hehao00 <13663474696@163.com>
2019-07-12 17:47:21 +0800

    动态路由组件缓存

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <!--
+      路由组件是动态的 默认情况下切换都会销毁重建 状态丢失
+      在切换的时候保留状态 则使用 keep-alive 缓存
+     -->
+    <keep-alive>
+       <router-view></router-view>
+    </keep-alive>
   </div>
 </template>
 
