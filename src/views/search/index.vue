b8a7700c3b8bb312c573ee9e7c93651aeeec32c1
hehao00 <13663474696@163.com>
2019-07-15 16:28:08 +0800

    搜索---关键字高亮

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -11,8 +11,14 @@
         icon="search"
         v-for="item in suggestions"
         :key="item"
-        :title="item"
-      />
+      >
+      <!--
+          {{}} 无法输出 html 字符内容
+          v-html 指令才会解析字符串中的 html
+          过滤只能用在{{}} 和 v-bind 中
+       -->
+        <div slot="title" v-html="hightlight(item, searchText)"></div>
+      </van-cell>
 
     </van-cell-group>
     <!-- /联想建议列表 -->
@@ -44,6 +50,10 @@ export default {
     }
   },
   watch: {
+    // debounce接收两个参数
+    // 第一个参数： 执行业务逻辑的参数函数
+    // 第二个参数： 防抖时间
+    // 当同一时间调用频率过快时，只有停下来经过指定的时间才会调用
     searchText: debounce(async function (newVal) {
       newVal = newVal.trim() // 去除首尾空格
       // 如果数据为空 则什么都不做
@@ -55,7 +65,11 @@ export default {
       this.suggestions = data.options
     }, 500)
   },
-  methods: {}
+  methods: {
+    hightlight (text, keyword) {
+      return text.toLowerCase().split(keyword).join(`<span style="color:red;">${keyword}</span>`)
+    }
+  }
 }
 </script>
 
