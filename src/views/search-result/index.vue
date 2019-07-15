ef70cfb34574fc82eff831b002a33e470d5d8393
hehao00 <13663474696@163.com>
2019-07-15 17:18:07 +0800

    搜索--导航搜索结果页面

---
+++ src/views/search-result/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>ss</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchResult',
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
